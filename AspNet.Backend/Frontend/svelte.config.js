efebdb4ec178ca1d2807fa023dfdaf4311ea0b81
genar <larsm0806@googlemail.com>
2025-10-02 13:39:18 +0200

    Frontend now deploys correctly when build & deployed with docker.

--- AspNet.Backend/Frontend/svelte.config.js
+++ AspNet.Backend/Frontend/svelte.config.js
@@ -1,14 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: path.resolve(new URL(import.meta.url).pathname, '../dist'), // Hier wird die Seite gebaut
-			assets: path.resolve(new URL(import.meta.url).pathname, '../dist'), // Assets werden hier abgelegt
-			fallback: 'index.html',    // Aktiviert den Fallback-Modus 
-		}),
+        adapter: adapter({
+            pages: 'dist',
+            assets: 'dist',
+            fallback: 'index.html',
+        }),
 		paths: {
 			base: '', // Falls du eine bestimmte Basis-URL hast, passe dies an
 		},
